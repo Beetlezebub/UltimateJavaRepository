8c9919dfa611c14b6bebafa818f2d02500a8abab
Beetle Freeman <1026kailey@gmail.com>
2024-01-11 10:25:19 -0600

    restored mysteriously removed files

--- Unit01/HelloPerson.java
+++ Unit01/HelloPerson.java
@@ -2,9 +2,8 @@ package Unit01;
 
 public class HelloPerson {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println(args[0]);
-
-        
+        double val = -0.7;
+        int roundedVal = (int)(val +0.5);
+        System.out.println(roundedVal);
     }
 }
