566695eada30d3abb7e41866cb4e86f148b98da6
Beetle Freeman <1026kailey@gmail.com>
2024-02-05 10:27:15 -0600

    Worked on assignment

--- Unit03/Xtras/QuadraticSolver.java
+++ Unit03/Xtras/QuadraticSolver.java
@@ -1,7 +1,21 @@
 package Unit03.Xtras;
 
+import java.util.Scanner;
+
 public class QuadraticSolver {
     public static void main(String[] args) {
+        Scanner coeff = new Scanner(System.in);
+
+        System.out.println("Enter the first coefficient (number).");
+        double num1 = coeff.nextDouble();
+
+        System.out.println("Enter the second coefficient (number).");
+        double num2 = coeff.nextDouble();
+
+        System.out.println("Enter the last coefficient (number).");
+        double num3 = coeff.nextDouble();
+
+        coeff.close();
 
     }
 }
