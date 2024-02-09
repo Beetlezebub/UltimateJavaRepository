ae0dde9b5b36ecad813c1eb764ca4828abbef216
Beetlezebub <155655920+Beetlezebub@users.noreply.github.com>
2024-02-09 15:36:33 +0000

    worked on quadratic solver

--- Unit03/Xtras/QuadraticSolver.java
+++ Unit03/Xtras/QuadraticSolver.java
@@ -1,22 +1,25 @@
 package Unit03.Xtras;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class QuadraticSolver {
     public static void main(String[] args) {
         Scanner coeff = new Scanner(System.in);
 
         System.out.println("Enter the first coefficient (number).");
-        double a = coeff.nextDouble();
+        int a = coeff.nextDouble();
 
         System.out.println("Enter the second coefficient (number).");
-        double b = coeff.nextDouble();
+        int b = coeff.nextDouble();
 
         System.out.println("Enter the last coefficient (number).");
-        double c = coeff.nextDouble();
+        int c = coeff.nextDouble();
 
         coeff.close();
 
+        int discriminant = Math.sqrt(Math.pow(b, 2) - 4*(a)*(c));
+        System.out.print(discriminant);
         //(-b(+-)-/(b)^2-4(a)(c))/2(a)
 
     }
