654c768e18a25f71acd710603a5efd3606e2a14a
Beetle Freeman <1026kailey@gmail.com>
2024-02-11 14:14:36 -0600

    Finished Unit 1 work

--- Unit01/Xtras/RandomStats.java
+++ Unit01/Xtras/RandomStats.java
@@ -3,17 +3,19 @@ package Unit01.Xtras;
 import java.lang.Math;
 
 public class RandomStats {
-    static void generate() {
-         double gen = (double) (Math.random()*2);
-         System.out.print(gen);
-    }
-
     public static void main(String[] args) {
-         double num1 = generate();
-         double num2 = generate();
-         double num3 = generate();
-         double num4 = generate();
-         double num5 = generate();
+          double gen1 = (double) (Math.random()*2);
+          double gen2 = (double) (Math.random()*2);
+          double gen3 = (double) (Math.random()*2);
+          double gen4 = (double) (Math.random()*2);
+          double gen5 = (double) (Math.random()*2);
+          double av = ((gen1 + gen2 + gen3 + gen4 + gen5)/5);
+          double min = (double) (Math.min(gen1, gen5));
+          double max = (double) (Math.max(gen1, gen5));
+          System.out.println(gen1 + "; " + gen2 + "; " + gen3 + "; " + gen4 + "; " + gen5);
+          System.out.println("Average: " + av);
+          System.out.println("Minimum: " + min + "; Max: " + max);
+
     }
 }
 // javac Unit01\Xtras\RandomStats.java; java Unit01.Xtras.RandomStats 27 93 94 76
--- Unit01/Xtras/TwoDSix.java
+++ Unit01/Xtras/TwoDSix.java
@@ -1,7 +1,12 @@
 package Unit01.Xtras;
 
+import java.lang.Math;
+
 public class TwoDSix {
     public static void main(String[] args) {
-
+        int roll1 = ((int) (Math.random()*6) + 1);
+        int roll2 = ((int) (Math.random()*6) + 1);
+        int total = roll1 + roll2;
+        System.out.println("You rolled a " + roll1 + "and a " + roll2 + ", for a total of " + total + ".");
     }
 }
