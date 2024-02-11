d5dff03563b66ca2ca34ac0029e78b5c3e7dd4a4
Beetle Freeman <1026kailey@gmail.com>
2024-02-11 15:39:17 -0600

    Finished unit 3 work

--- Unit03/Xtras/BasicTextAnalysis.java
+++ Unit03/Xtras/BasicTextAnalysis.java
@@ -1,8 +1,26 @@
 package Unit03.Xtras;
 
+import java.util.Scanner;
+
 public class BasicTextAnalysis {
     public static void main(String[] args) {
 
+        Scanner tana = new Scanner(System.in);
+
+        System.out.println("Enter your text:");
+        String text = tana.nextLine();
+
+        tana.close();
+
+        int numchar = text.length();
+        int spcin = text.indexOf(" ");
+        String firstWord = text.substring(0, spcin);
+        int perin = text.indexOf(".");
+        String firstSentence = text.substring(0, perin);
+        
+        System.out.println("Number of Characters: " + numchar);
+        System.out.println("First Word: " + firstWord);
+        System.out.println("First Sentence: " + firstSentence);
     }
 }
 
