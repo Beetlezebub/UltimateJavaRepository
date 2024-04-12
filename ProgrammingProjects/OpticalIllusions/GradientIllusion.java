9176a81f551dac0188d2ba8f7e8d235c13ee92ce
Beetle Freeman <1026kailey@gmail.com>
2024-04-12 10:12:07 -0500

    trying out visual stuff

--- ProgrammingProjects/OpticalIllusions/GradientIllusion.java
+++ ProgrammingProjects/OpticalIllusions/GradientIllusion.java
@@ -8,13 +8,17 @@ public class GradientIllusion extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        
+        //Draw the rectangle and fill
+        g.fillRect(100, 256, 400, 200);
+
 
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("GradientIllusion");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        frame.setSize(512, 512);
         frame.add(new GradientIllusion());
         frame.setVisible(true);
     }
